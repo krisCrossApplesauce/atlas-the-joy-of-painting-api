e8719dc33b631493aaed5106f56968c309b0b9ae
kris_cross_applesauce <karisrr2203@gmail.com>
2024-03-26 15:47:22 -0500

    0: more than one table to make it easier to access the information

--- create_db.sql
+++ create_db.sql
@@ -1,7 +1,50 @@
 CREATE TABLE "episodes" (
   "id" integer PRIMARY KEY,
   "title" varchar,
+  "air_date" varchar,
   "month" varchar,
   "colors" varchar,
   "subjects" varchar
 );
+
+CREATE TABLE "months" (
+  "id" integer PRIMARY KEY,
+  "name" varchar,
+  "episodes" varchar
+);
+
+CREATE TABLE "colors" (
+  "id" integer PRIMARY KEY,
+  "name" varchar,
+  "episodes" varchar
+);
+
+CREATE TABLE "subjects" (
+  "id" integer PRIMARY KEY,
+  "name" varchar,
+  "episodes" varchar
+);
+
+ALTER TABLE "months" ADD FOREIGN KEY ("episodes") REFERENCES "episodes" ("month");
+
+CREATE TABLE "episodes_colors" (
+  "episodes_colors" varchar,
+  "colors_episodes" varchar,
+  PRIMARY KEY ("episodes_colors", "colors_episodes")
+);
+
+ALTER TABLE "episodes_colors" ADD FOREIGN KEY ("episodes_colors") REFERENCES "episodes" ("colors");
+
+ALTER TABLE "episodes_colors" ADD FOREIGN KEY ("colors_episodes") REFERENCES "colors" ("episodes");
+
+
+CREATE TABLE "episodes_subjects" (
+  "episodes_subjects" varchar,
+  "subjects_episodes" varchar,
+  PRIMARY KEY ("episodes_subjects", "subjects_episodes")
+);
+
+ALTER TABLE "episodes_subjects" ADD FOREIGN KEY ("episodes_subjects") REFERENCES "episodes" ("subjects");
+
+ALTER TABLE "episodes_subjects" ADD FOREIGN KEY ("subjects_episodes") REFERENCES "subjects" ("episodes");
+
---
+++ not_create_db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "episodes" (
+  "id" integer PRIMARY KEY,
+  "title" varchar,
+  "month" varchar,
+  "colors" varchar,
+  "subjects" varchar
+);
